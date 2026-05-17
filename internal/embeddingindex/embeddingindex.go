18f84ac39f2ce4dbe8eaf2c1b96bcf2d6d90f291
Finn Agethen <finn.agethen@gmail.com>
2026-05-17 20:51:57 +0200

    EmbeddingIndex code structure

--- internal/embeddingindex/embeddingindex.go
+++ internal/embeddingindex/embeddingindex.go
@@ -1,1 +1,33 @@
 package embeddingindex
+
+type EmbeddingIndex struct {
+	Vocab              map[string]int // token -> token index
+	TokenEmbedings     []float32      // token index -> embedding
+	DocumentEmbeddings []float32      // document index -> embedding
+}
+
+func NewEmbeddingIndex() *EmbeddingIndex {
+	return &EmbeddingIndex{
+		Vocab: make(map[string]int),
+	}
+}
+
+func (index *EmbeddingIndex) LoadEmbeddings(path string) error {
+	return nil
+}
+
+func (index *EmbeddingIndex) BuildFromDocuments(documents []string) error {
+	return nil
+}
+
+func (index *EmbeddingIndex) EmbedDocument(document string) []float32 {
+	return nil
+}
+
+func (index *EmbeddingIndex) CosineSimilarity(a, b []float32) float32 {
+	return 0
+}
+
+func (index *EmbeddingIndex) TopKNeighbors(document []float32, k int) []int {
+	return nil
+}
